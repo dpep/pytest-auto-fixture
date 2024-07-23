a92885e44a73f083ceccb3c1d103f076d7946e96
Daniel Pepper <pepper.daniel@gmail.com>
2024-07-23 12:51:19 -0700

    local dev

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
 # pytest_plugins = ["pytest_auto_fixture"]
+
+# for local development, manually register plugin
+def pytest_configure(config):
+    if not config.pluginmanager.hasplugin("auto_fixture"):
+        import pytest_auto_fixture
+        config.pluginmanager.register(pytest_auto_fixture)
